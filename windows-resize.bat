2947ea759841f3863e1f74697f280191eb32dfdc
Nana Sulis <111860707+enamribu@users.noreply.github.com>
2026-03-30 22:26:57 +0700

    Update windows-resize.bat

--- windows-resize.bat
+++ windows-resize.bat
@@ -1,6 +1,7 @@
 @echo off
 net accounts /lockoutthreshold:0
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v InstallChrome /t REG_SZ /d "powershell -ExecutionPolicy Bypass -Command \"$url='https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi'; $dst='C:\Windows\Temp\chrome.msi'; Invoke-WebRequest -Uri $url -OutFile $dst; Start-Process msiexec.exe -ArgumentList '/i `\"' + $dst + '`\" /qn /norestart' -Wait\"" /f
 mode con cp select=437 >nul
 
 set C=%SystemDrive:~0,1%
